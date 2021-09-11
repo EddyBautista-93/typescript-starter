d237349dd2408c1b0d2e8833b7a0f6abb2011b90
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-11 10:28:02 -0500

    notes on types

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,19 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
 // character = 30; type number is not assignable to type 'string'
 character = 'Eddy';
+
 // age = '30'; string not assignable to number 
 age = 28;
+
 // isBlackBelt = 'yes'; string is not assignable to boolean 
 isBlackBelt = true;
-var circ = function (diameter) {
+
+const circ = (diameter: number) => {
     return diameter * Math.PI;
-};
+}
+
 console.log(circ(32));
+
